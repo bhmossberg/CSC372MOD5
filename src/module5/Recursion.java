5eded527ac6f32430af38f0e68efd725e956328b
bhmossberg <benjamin.h.mossberg@gmail.com>
2026-06-08 14:38:23 -0400

    Recursion v1.1

--- src/module5/Recursion.java
+++ src/module5/Recursion.java
@@ -1,21 +1,34 @@
 package module5;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Recursion {
     
     // Recursive method to calculate product of 'count' numbers
-    public static int calculateProduct(Scanner sc, int remaining) {
-        // Base case: only 1 number left to read
-        if (remaining == 1) {
+	public static long calculateProduct(Scanner sc, int remaining) {
+        long num = 0;
+        boolean validInput = false;
+        
+        while (!validInput) {
             System.out.print("Enter number " + (6 - remaining) + ": ");
-            return sc.nextInt();
+            
+            try {
+                num = sc.nextLong();
+                validInput = true;
+            } 
+            catch (InputMismatchException e) {
+                System.out.println("Invalid input! Please enter a whole number (integer).");
+                sc.nextLine();
+            }
         }
         
-        // Recursive case: read current number and multiply by product of remaining numbers
-        System.out.print("Enter number " + (6 - remaining) + ": ");
-        int num = sc.nextInt();
+        // Base case: only 1 number left to read
+        if (remaining == 1) {
+            return num;
+        }
         
+        // Recursive case: multiply current number by product of remaining numbers
         return num * calculateProduct(sc, remaining - 1);
     }
 
@@ -26,7 +39,7 @@ public class Recursion {
         System.out.println("Please enter five integers:\n");
         
         // Start recursion with 5 numbers to read
-        int product = calculateProduct(scanner, 5);
+        long product = calculateProduct(scanner, 5);
         
         System.out.println("\nThe product of all five numbers is: " + product);
         
